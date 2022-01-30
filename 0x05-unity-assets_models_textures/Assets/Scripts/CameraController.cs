5e01cfee79cab334245539df139202042d522a4f
daniapm <daniajibellis@hotmail.com>
2022-01-30 13:46:46 -0600

    Sript  CameraController allows the camera to follow the player.

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initial position of Camera controller
+        transform.position = new Vector3(22, 26, 7);
+        // Calculate and store the offset value by getting the distance
+        // between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+        // offset(x,y,z) stored => -1, 24.8, -9
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Fixed the Camera position
+        transform.position = player.transform.position + offset ;
+        
+    }
+}
